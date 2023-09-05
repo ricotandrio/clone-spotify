2df5743a320156c09bab457267f4f8de6288c2b0
ricotandrio <ricotandrio@gmail.com>
2023-09-05 09:08:51 +0700

    update: fix fetch local data

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -24,12 +24,14 @@ import QueryProvider from '../context/QueryContext.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 import Artists from '../../pages/Artists.jsx';
 
+import userdata from '../../public/datas.json';
+
 export default function RouterRedirect() {
 
   // local json-server data
-  const [userdatas, setuserData] = useState({data: [], isLoading: true, errorMessage: ''});
+  const [userdatas, setuserData] = useState({data: userdata, isLoading: false, errorMessage: ''});
   // fetchdata('http://localhost:3000/users', setuserData);
-  fetchdata('../../public/datas.json', setuserData);
+  // fetchdata('../../public/datas.json', setuserData);
 
   const [favorite, setFavorite] = useState([]);
   // favorite data
